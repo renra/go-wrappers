0023451e2dd259da0b42c14b3ed16f19e72a16c2
Jan Gloser <jan.renra.gloser@gmail.com>
2019-02-14 15:33:55 +0100

    Working examples

---
+++ wrappers/main.go
@@ -0,0 +1,41 @@
+package wrappers
+
+import (
+  "github.com/renra/go-logger/logger"
+  "github.com/renra/go-pseudoglobals/pseudoglobals"
+  "github.com/renra/go-json-http-handler/jsonHttpHandler"
+)
+
+type LoggerWrapper struct {
+}
+
+func (lw LoggerWrapper) New(service string, severity int, severities map[int]string) pseudoglobals.LoggerInstance {
+  return &logger.Logger{
+    Label: service,
+    ThresholdSeverity: severity,
+    Severities: severities,
+    Serialize: logger.GCPSerialize,
+  }
+}
+
+type GlobalsWrapper struct {
+  Globals *pseudoglobals.Pseudoglobals
+}
+
+func (gw *GlobalsWrapper) Config() jsonHttpHandler.Config {
+  return gw.Globals.Config()
+}
+
+func (gw *GlobalsWrapper) Logger() jsonHttpHandler.Logger {
+  return gw.Globals.Logger()
+}
+
+func (gw *GlobalsWrapper) Log(msg string) {
+  gw.Globals.Log(msg)
+}
+
+func (gw *GlobalsWrapper) LogErrorWithTrace(err string, trace string) {
+  gw.Globals.LogErrorWithTrace(err, trace)
+}
+
+
