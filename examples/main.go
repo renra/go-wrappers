9eab4dfcb50a1ae38f4f569544d655cf83c8d98d
Jan Gloser <jan.renra.gloser@gmail.com>
2019-03-18 12:54:43 +0100

    Updated libraries

--- examples/main.go
+++ examples/main.go
@@ -3,6 +3,7 @@ package main
 import (
   "fmt"
   "app/wrappers"
+  "github.com/renra/go-errtrace/errtrace"
   "github.com/renra/go-pseudoglobals/pseudoglobals"
   "github.com/renra/go-json-http-handler/jsonHttpHandler"
   "database/sql"
@@ -11,12 +12,44 @@ import (
 type Config struct {
 }
 
-func (c *Config) Get(msg string) interface{} {
-  return msg
+func (ci *Config) Get(key string) (interface{}, *errtrace.Error) {
+  return key, nil;
 }
 
-func (c *Config) GetString(msg string) string {
-  return msg
+func (ci *Config) GetP(key string) interface{} {
+  return key;
+}
+
+func (ci *Config) GetString(key string) (string, *errtrace.Error) {
+  return key, nil;
+}
+
+func (ci *Config) GetStringP(key string) string {
+  return key;
+}
+
+func (ci *Config) GetInt(key string) (int, *errtrace.Error) {
+  return 4, nil;
+}
+
+func (ci *Config) GetIntP(key string) int {
+  return 4;
+}
+
+func (ci *Config) GetFloat(key string) (float64, *errtrace.Error) {
+  return 3.14, nil;
+}
+
+func (ci *Config) GetFloatP(key string) float64 {
+  return 3.14;
+}
+
+func (ci *Config) GetBool(key string) (bool, *errtrace.Error) {
+  return true, nil;
+}
+
+func (ci *Config) GetBoolP(key string) bool {
+  return true;
 }
 
 func main() {
@@ -28,10 +61,9 @@ func main() {
   })
 
   globals.Log("I have a logger")
-  globals.Log(fmt.Sprintln("And I have a config too: %s", globals.Config().GetString("whatever")))
+  globals.Log(fmt.Sprintln("And I have a config too: %s", globals.Config().GetStringP("whatever")))
 
   globalsWrapper := wrappers.GlobalsWrapper{Globals: globals}
-  globalsWrapper.Log(fmt.Sprintln("And I have a db client: %v", globalsWrapper.DB("postgres")))
   globalsWrapper.Log(fmt.Sprintln("And I could have many more clients too:: %v", globalsWrapper.Clients()))
 
   handler := jsonHttpHandler.New(
